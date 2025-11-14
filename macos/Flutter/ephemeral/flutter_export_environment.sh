f00c0edf85d96bc7ac2572603fb552b9081dea7e
leapdeck <leapdeck1@gmail.com>
2025-11-13 18:13:33 -0800

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/modview/Documents/ProjectsHub/IDEHub1/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/modview/Documents/ProjectsHub/CodingProjs/ProjCrsrSUI-Project26/ClothsConnect"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/modview/Documents/ProjectsHub/CodingProjs/ProjCrsrSUI-Project26/ClothsConnect/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzEuMC0wLjEucHJl,RkxVVFRFUl9DSEFOTkVMPWJldGE=,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZmNlODA3NTMwYQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NTBmMjI2NTY5Zg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjAgKGJ1aWxkIDMuOC4wLTE3MS4wLmRldik="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/modview/Documents/ProjectsHub/CodingProjs/ProjCrsrSUI-Project26/ClothsConnect/.dart_tool/package_config.json"
